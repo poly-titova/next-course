efb7bd4d69d0b3558fbda97c41064e568af6a0f0
Poly Titova <poly_titova@mail.ru>
2020-12-28 21:52:10 +0300

    Добавили meta

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Index() {
-  return <>
-    <h1>Hello Next.JS!</h1>
-    <p><Link href={'/about'}><a>About</a></Link></p>
-    <p><Link href={'/posts'}><a>Posts</a></Link></p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-  </>
+  return (
+    <>
+      <Head>
+        <title>Next Title Index Hello</title>
+      </Head>
+      <h1>Hello Next.JS!</h1>
+      <p><Link href={'/about'}><a>About</a></Link></p>
+      <p><Link href={'/posts'}><a>Posts</a></Link></p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+    </>
+  )
 }
--- pages/posts.js
+++ pages/posts.js
@@ -1,3 +1,15 @@
+import Head from 'next/head'
+
 export default function Posts() {
-  return <h1>Posts Page</h1>
+  return (
+    <>
+      <Head>
+        <title>Posts Page | Next Course</title>
+        <meta name="keywords" content="next,javascript,nextjs,react" />
+        <meta name="description" content="this is youtube tutorial for next" />
+        <meta charset="utf-8" />
+      </Head>
+      <h1>Posts Page</h1>
+    </>
+  )
 }
