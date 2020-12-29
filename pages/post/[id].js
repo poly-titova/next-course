c849cd4a0d9913387433b202ee16a2bc2e038089
Poly Titova <poly_titova@mail.ru>
2020-12-29 20:26:05 +0300

    Сделали вывод постов

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,12 +1,24 @@
-import { useRouter } from 'next/router'
-import {MainLayout} from '../../components/MainLayout'
+import { MainLayout } from '../../components/MainLayout'
+import Link from 'next/link'
 
-export default function Post() {
-  const router = useRouter()
-  console.log(router)
+export default function Post({ post }) {
   return (
     <MainLayout>
-      <h1>Post {router.query.id}</h1>
+      <h1>{post.title}</h1>
+      <hr />
+      <p>{post.body}</p>
+      <Link href={'/posts'}><a>Back to all posts</a></Link>
     </MainLayout>
   )
+}
+
+Post.getInitialProps = async (ctx) => {
+  console.log(ctx.query)
+
+  const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`)
+  const post = await response.json()
+
+  return {
+    post
+  }
 }
--- pages/posts.js
+++ pages/posts.js
@@ -1,23 +1,28 @@
-import { useState, useEffect } from 'react'
 import { MainLayout } from '../components/MainLayout'
+import Link from 'next/link'
 
-export default function Posts() {
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    async function load() {
-      const response = await fetch('http://localhost:4200/posts')
-      const json = await response.json()
-      setPosts(json)
-    }
-
-    load()
-  }, [])
-
+export default function Posts({ posts }) {
   return (
     <MainLayout title={'Posts Page'}>
       <h1>Posts Page</h1>
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <Link href={`/post/${post.id}`} as={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </MainLayout>
   )
+}
+
+Posts.getInitialProps = async () => {
+  const response = await fetch('http://localhost:4200/posts')
+  const posts = await response.json()
+
+  return {
+    posts
+  }
 }
